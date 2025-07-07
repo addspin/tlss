eaa9d1f8e6d358106e7f881aad47d7633b5718c3
tdv <lab137@yandex.ru>
2025-07-07 16:36:13 +0300

    Добавлено новое меню в пользоватльских сертификатов, переработан шаблон.

--- check/tcp.go
+++ check/tcp.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"strconv"
 	"sync"
 	"time"
 
 	"github.com/addspin/tlss/models"
+	"github.com/addspin/tlss/utils"
 	"github.com/jmoiron/sqlx"
 	"github.com/spf13/viper"
 )
@@ -30,10 +30,10 @@ func (s *StatusCodeTcp) TCPPortAvailable(timeTicker time.Duration) {
 
 	db, err := sqlx.Open("sqlite3", database)
 	if err != nil {
-		log.Printf("Error open database: %v", err)
+		log.Printf("Ошибка открытия базы данных: %v", err)
 		return
 	}
-	fmt.Println("Connected to database: ", database)
+	fmt.Println("Подключено к базе данных: ", database)
 	defer db.Close()
 
 	var checkServerList bool
@@ -41,38 +41,43 @@ func (s *StatusCodeTcp) TCPPortAvailable(timeTicker time.Duration) {
 		// проверяем, есть хотя бы один сервер в базе данных?
 		err = db.Get(&checkServerList, "SELECT EXISTS(SELECT 1 FROM server)")
 		if err != nil {
-			log.Printf("Error checking server list: %v", err)
+			log.Printf("Ошибка проверки списка серверов: %v", err)
 			continue
 		}
 		// если нет, пишем что в базе нет серверов
 		if !checkServerList {
-			log.Println("No servers in database")
+			log.Println("В базе данных нет серверов")
 			continue
 		}
 		// извлекаем список северов из базы данных
 		serverList := []models.Server{}
 		err = db.Select(&serverList, "SELECT hostname, port FROM server")
 		if err != nil {
-			log.Printf("Error selecting servers: %v", err)
+			log.Printf("Ошибка выбора серверов: %v", err)
 			continue
 		}
 		// проходимся по списку серверов и проверяем доступность
+		testData := utils.NewTestData()
+		port, err := testData.TestString(serverList[0].Port)
+		if err != nil {
+			log.Printf("Ошибка преобразования порта: %v", err)
+			continue
+		}
 		for _, server := range serverList {
-			port := strconv.Itoa(server.Port)
 			conn, err := net.Dial("tcp", server.Hostname+":"+port)
 			if err != nil {
 				s.ExitCodeTcp = false // port is not available
 				// log.Println("TCP port is not available:", server.Hostname+":"+server.Port)
-				log.Printf("TCP port is not available: %s:%s, error: %v", server.Hostname, server.Port, err)
+				log.Printf("TCP порт не доступен: %s:%d, ошибка: %v", server.Hostname, server.Port, err)
 				_, err = db.Exec("UPDATE server SET server_status = ? WHERE hostname = ? AND port = ?", "offline", server.Hostname, server.Port)
 				if err != nil {
-					log.Printf("Error updating server status: %v", err)
+					log.Printf("Ошибка обновления статуса сервера: %v", err)
 					continue
 				}
 			} else {
 				s.ExitCodeTcp = true // port is available
 				// log.Println("TCP port is available:", server.Hostname+":"+server.Port)
-				log.Printf("TCP port is available: %s:%s", server.Hostname, server.Port)
+				log.Printf("TCP порт доступен: %s:%d", server.Hostname, server.Port)
 				// меняем значение в базе данных c offline на online
 				_, err = db.Exec("UPDATE server SET server_status = ? WHERE hostname = ? AND port = ?", "online", server.Hostname, server.Port)
 				if err != nil {

8c9803610d697fc4408faa16736a79a071871fab
addspin <lab137@ya.ru>
2025-09-12 09:28:44 +0400

    update: init mon for checkers

---
+++ check/mon.go
@@ -0,0 +1,28 @@
+package check
+
+import (
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+type Monitor struct {
+	RecreateCerts         time.Time
+	CheckValidCerts       time.Time
+	CheckTCP              time.Time
+	RecreateCertStatus    bool
+	CheckValidCertsStatus bool
+	CheckTCPStatus        bool
+}
+
+func (m *Monitor) CheckMonitor() {
+
+	recreateCertsInterval := viper.GetInt("recreateCerts.time")
+	recreateCertsTimeNow := time.Now()
+	recreateDuration := recreateCertsTimeNow.Sub(m.RecreateCerts)
+	if recreateDuration > recreateCertsInterval {
+		m.RecreateCertStatus = true
+	} else {
+		m.RecreateCertStatus = false
+	}
+}

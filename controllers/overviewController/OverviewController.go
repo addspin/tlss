9b0ae67db1da947d7879f4bdf5a408947f47bd70
addspin <lab137@ya.ru>
2026-05-08 11:54:50 +0400

    update: overview pages - view only no valid status - add servers cert expired in 30 day (default) - add clients cert expired in 30 day (default) add: in config overview time settings

--- controllers/overviewController/OverviewController.go
+++ controllers/overviewController/OverviewController.go
@@ -74,6 +74,116 @@ func Overview(c fiber.Ctx) error {
 			slog.Error("Fatal error", "error", err)
 		}
 
+		// Только невалидные статусты клиентских сертификатов
+		userCertNoValidList := []models.UserCertsData{}
+		err = db.Select(&userCertNoValidList, "SELECT * FROM user_certs WHERE cert_status != ?", certStatusValid)
+		if err != nil {
+			slog.Error("Fatal error", "error", err)
+		}
+
+		// Только невалидные статусты серверных сертификатов
+		serverCertNoValidList := []models.CertsData{}
+		err = db.Select(&serverCertNoValidList, "SELECT * FROM certs WHERE cert_status != ?", certStatusValid)
+		if err != nil {
+			slog.Error("Fatal error", "error", err)
+		}
+
+		checkClientExpireDaysLeft := viper.GetInt("overview.checkClientExpireDaysLeft")
+		// только валидные клиентские сертификаты
+		type UserCertsData struct {
+			Id             int    `db:"id"`
+			EntityId       int    `db:"entity_id"`
+			CommonName     string `db:"common_name"`
+			DaysLeft       int    `db:"days_left"`
+			CertExpireTime string `db:"cert_expire_time"`
+			CertStatus     int    `db:"cert_status"`
+		}
+		userCertValidList := []UserCertsData{}
+
+		type userCertExpiredTimeList struct {
+			Id         int    `db:"id"`
+			EntityName string `db:"entity_name"`
+			CommonName string `db:"common_name"`
+			DaysLeft   int    `db:"days_left"`
+		}
+
+		userCertExpiredList := []userCertExpiredTimeList{}
+		err = db.Select(&userCertValidList, "SELECT  id, entity_id, common_name, days_left, cert_expire_time FROM user_certs WHERE cert_status = ?", certStatusValid)
+		if err != nil {
+			slog.Error("Fatal error", "error", err)
+		}
+		for _, cert := range userCertValidList {
+			//  получаем имя сервера из server_id
+			var entity_name []string
+			err = db.Select(&entity_name, "SELECT entity_name FROM entity WHERE id = ?", cert.EntityId)
+			if err != nil {
+				slog.Error("Fatal error", "error", err)
+				continue
+			}
+			if len(entity_name) == 0 {
+				slog.Error("ENTITY NOT FOUND", "id", cert.EntityId, "entity", cert.CommonName)
+				continue
+			}
+			//узнаем сколько дней до истечения сертификата, сравниваем и записываем в список
+			if cert.DaysLeft <= checkClientExpireDaysLeft {
+				newItem := userCertExpiredTimeList{
+					Id:         cert.Id,
+					EntityName: entity_name[0],
+					CommonName: cert.CommonName,
+					DaysLeft:   cert.DaysLeft,
+				}
+				userCertExpiredList = append(userCertExpiredList, newItem)
+			}
+		}
+
+		// только валидные серверные сертификаты
+		checkServerExpireDaysLeft := viper.GetInt("overview.checkServerExpireDaysLeft")
+		type CertsData struct {
+			Id             int    `db:"id"`
+			ServerId       int    `db:"server_id"`
+			Domain         string `db:"domain"`
+			DaysLeft       int    `db:"days_left"`
+			CertExpireTime string `db:"cert_expire_time"`
+			CertStatus     int    `db:"cert_status"`
+		}
+		serverCertValidList := []CertsData{}
+		type serverCertExpiredTimeList struct {
+			Id       int    `db:"id"`
+			Hostname string `db:"hostname"`
+			Domain   string `db:"domain"`
+			DaysLeft int    `db:"days_left"`
+		}
+		serverCertExpiredList := []serverCertExpiredTimeList{}
+		// Извлекаем только валилные серты
+		err = db.Select(&serverCertValidList, "SELECT id, server_id, domain, days_left, cert_expire_time FROM certs WHERE cert_status = ?", certStatusValid)
+		if err != nil {
+			slog.Error("Fatal error", "error", err)
+		}
+		for _, cert := range serverCertValidList {
+			//  получаем имя сервера из server_id
+			var hostname []string
+			err = db.Select(&hostname, "SELECT hostname FROM server WHERE id = ?", cert.ServerId)
+			if err != nil {
+				slog.Error("Fatal error", "error", err)
+				continue
+			}
+			if len(hostname) == 0 {
+				slog.Error("SERVER NOT FOUND", "id", cert.ServerId, "domain", cert.Domain)
+				continue
+			}
+
+			//узнаем сколько дней до истечения сертификата, сравниваем и записываем в список
+			if cert.DaysLeft <= checkServerExpireDaysLeft {
+				newItem := serverCertExpiredTimeList{
+					Id:       cert.Id,
+					Hostname: hostname[0],
+					Domain:   cert.Domain,
+					DaysLeft: cert.DaysLeft,
+				}
+				serverCertExpiredList = append(serverCertExpiredList, newItem)
+			}
+		}
+
 		userCertCount := 0
 		userCertExpired := 0
 		userCertRevoked := 0
@@ -96,18 +206,24 @@ func Overview(c fiber.Ctx) error {
 		taskList := map[string]bool{"Recreate certs": recreateCertCheck, "Valid certs": validCertsCheck, "Server check": tcpCheck}
 
 		data := fiber.Map{
-			"Title":             "Overview",
-			"serverList":        &serverList,
-			"taskList":          &taskList,
-			"caCertList":        &caCertList,
-			"userCertList":      &userCertList,
-			"userCertCount":     userCertCount,
-			"userCertExpired":   userCertExpired,
-			"userCertRevoked":   userCertRevoked,
-			"serverCertCount":   serverCertCount,
-			"serverCertExpired": serverCertExpired,
-			"serverCertRevoked": serverCertRevoked,
-			"serverCertList":    &serverCertList,
+			"Title":                     "Overview",
+			"serverList":                &serverList,
+			"taskList":                  &taskList,
+			"caCertList":                &caCertList,
+			"userCertList":              &userCertList,
+			"userCertNoValid":           &userCertNoValidList,
+			"serverCertNoValid":         &serverCertNoValidList,
+			"userCertCount":             userCertCount,
+			"userCertExpired":           userCertExpired,
+			"userCertRevoked":           userCertRevoked,
+			"serverCertCount":           serverCertCount,
+			"serverCertExpired":         serverCertExpired,
+			"serverCertRevoked":         serverCertRevoked,
+			"serverCertList":            &serverCertList,
+			"userCertExpiredList":       &userCertExpiredList,
+			"serverCertExpiredList":     &serverCertExpiredList,
+			"checkServerExpireDaysLeft": checkServerExpireDaysLeft,
+			"checkClientExpireDaysLeft": checkClientExpireDaysLeft,
 		}
 
 		// Проверяем, является ли запрос HTMX запросом
